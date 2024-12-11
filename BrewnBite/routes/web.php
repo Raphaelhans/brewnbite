47b9d0f0c0cac0cfe166b169a956b4539a514539
Darren Cahya Wijaya <cosmicbro08@gmail.com>
2024-12-12 00:36:06 +0700

    #4 Added admin pages route

--- BrewnBite/routes/web.php
+++ BrewnBite/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/login');
+
+Route::get('/login', [AdminController::class, 'login'])->name('login');
+// Route User
+
+// Route Karyawan
+
+// Route Admin
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/inventory', [AdminController::class, 'inventory'])->name('inventory');
+    Route::get('/ratings', [AdminController::class, 'ratings'])->name('ratings');
+    Route::get('/sales', [AdminController::class, 'sales'])->name('sales');
+    Route::get('/bestsellers', [AdminController::class, 'bestsellers'])->name('bestsellers');
 });
