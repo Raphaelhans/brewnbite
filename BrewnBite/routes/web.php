59f5ca1d87bd3fc3b5a975479cd1d738a573b513
Darren Cahya Wijaya <cosmicbro08@gmail.com>
2024-12-12 12:00:03 +0700

    #4 Route fix

--- BrewnBite/routes/web.php
+++ BrewnBite/routes/web.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::redirect('/', '/login');
 
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::post('/login',[AuthController::class, 'login']);
 Route::post('/register',[AuthController::class, 'register']);
 Route::get('/logout',[AuthController::class, 'logout']);
